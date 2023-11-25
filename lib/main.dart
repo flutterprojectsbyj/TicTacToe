0f133337256cc21563d85e307649314d79b789fd
jmpfbmx <jmpf.bmx@gmail.com>
2023-11-25 19:03:52 +0100

    TicTacToe: Initial empty project
    Change-Id: I3e92c0d4dde1c17839f9fe4d513f26831d97bd76
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tic Tac Toe',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true
+      ),
+      themeMode: ThemeMode.dark,
+      home: const Scaffold(),
+    );
+  }
+}
